4657c5f460fedcec63c43e60960e29b26c395c90
Shipra <Shipraa94@gmail.com>
2023-12-14 12:04:34 +0530

    Added top rated filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,28 @@
 import ResCard from "./ResCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
+
 const Body = () =>{
+    const [restaurantList, setRestaurantList] = useState(resList);
     return (
         <div className="body">
-        <div className="search">Search</div>
+        <div className="button" >
+            <button onClick={()=>{
+                const tempRestList = restaurantList.filter((res)=>{
+                   return res?.info?.avgRating > 4; 
+                })
+                setRestaurantList(tempRestList);
+    
+            }}>Top Rated Restaurant
+            </button>
+            <button style={{marginLeft: '10px'}} onClick={()=>{
+                setRestaurantList(resList);
+            }}>See All Restaurant</button>
+        </div>
         <div className="res-container">
             {
-            resList.map((restaurant)=>{
+            restaurantList.map((restaurant)=>{
                 return <ResCard key={restaurant?.info?.id} resData={restaurant}/>
             })
             }
